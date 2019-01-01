ecd5c7ea6bc146131d6e50101030654fee4bac3c
Bukharkin Sergey <buharkin@selectel.ru>
2019-01-01 16:17:20 +0300

    Finished work

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <cmath>
 
+//(V*t%s+s)%s
+//S - длина окружности(км);
+//V - скорость(км/час);
+//T - время(час);
 int main()
 {
-    // put your code here
-    unsigned int h1 = 0, i1 = 0, s1 = 0, sec1 = 0;
-    unsigned int h2 = 0, i2 = 0, s2 = 0, sec2 = 0;
-    std::cin >> h1 >> i1 >> s1 >> h2 >> i2 >> s2;
+    const int MKAD_LENGNH = 109;
+    int v = 0;
+    unsigned int vm = 0, t = 0, res = 0, dist = 0;
+    std::cin >> v >> t;
 
-    sec1 = h1 * 3600 + i1 * 60 + s1;
-    sec2 = h2 * 3600 + i2 * 60 + s2;
-
-    //std::printf("sec1:%d\nsec2:%d\n", sec1, sec2);
-    std::printf("%d", sec2 - sec1);
+    vm = std::abs(v);
+    dist = (vm * t % MKAD_LENGNH + MKAD_LENGNH) % MKAD_LENGNH;
+    //std::printf("Dist: %d\n", 0);
+    if (dist == 109 || dist == -109 || dist == 0)
+    {
+        std::printf("%d", 0);
+    }
+    else if (v < 0)
+    {
+        std::printf("%d", MKAD_LENGNH - dist);
+    }
+    else
+    {
+        std::printf("%d", dist);
+    }
     return 0;
 }
