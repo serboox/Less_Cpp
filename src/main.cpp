b8939a592cd500d420c3fe7af7658b7e50bd52b1
Bukharkin Sergey <buharkin@selectel.ru>
2019-01-01 13:22:55 +0300

    Finished work

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,49 @@
 int main()
 {
     // put your code here
-    int a = 0, b = 0, c = 0, r = 0, k = 0;
-    std::cin >> a >> b >> c;
-    r = a * c;
-    k = b * c;
-    if ((k % 100) == 0)
+    unsigned int h = 0, i = 0, s = 0, sec = 0;
+    std::cin >> sec;
+
+    if ((sec % 86400) >= 0)
     {
-        r += k / 100;
-        k = 0;
+        sec -= (sec / 86400) * 86400;
     }
-    else if (k > 100)
+
+    //std::printf("%d = %d %% %d\n", sec / 3600, sec, 3600);
+    if ((sec % 3600) >= 0)
     {
-        r += k / 100;
-        k = k % 100;
+        h += sec / 3600;
+        sec -= h * 3600;
     }
-    std::cout << r << " " << k << std::endl;
+
+    //std::printf("%d = %d %% %d\n", sec / 60, sec, 60);
+    if ((sec % 60) >= 0)
+    {
+        i += sec / 60;
+        sec -= i * 60;
+    }
+    if (sec > 0)
+    {
+        s += sec;
+    }
+
+    std::printf("%d:", h);
+    if (i >= 10)
+    {
+        std::printf("%d:", i);
+    }
+    else
+    {
+        std::printf("0%d:", i);
+    }
+
+    if (s >= 10)
+    {
+        std::printf("%d", s);
+    }
+    else
+    {
+        std::printf("0%d", s);
+    }
     return 0;
 }
