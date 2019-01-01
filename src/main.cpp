76a66153d000c19373886a567239b780b7b1a3bb
Bukharkin Sergey <buharkin@selectel.ru>
2019-01-01 12:14:25 +0300

    Finished work

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // put your code here
+    int a = 0, b = 0, c = 0, r = 0, k = 0;
+    std::cin >> a >> b >> c;
+    r = a * c;
+    k = b * c;
+    if ((k % 100) == 0)
+    {
+        r += k / 100;
+        k = 0;
+    }
+    else if (k > 100)
+    {
+        r += k / 100;
+        k = k % 100;
+    }
+    std::cout << r << " " << k << std::endl;
     return 0;
 }
