a74b607d6c1dbb702a55cb53c667cc7c7a8937b4
Bukharkin Sergey <buharkin@selectel.ru>
2019-01-03 00:09:08 +0300

    Finished work

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,17 @@
 int main()
 {
     size_t x = 0;
-    std::cin >> x;
-    double i = 0;
+    size_t i = 0;
     while (true)
     {
-        if (std::pow(2, i) == x)
-        {
-            std::printf("YES");
-            return 0;
-        }
-
-        if (i > 4096)
+        std::cin >> x;
+        if (x == 0)
         {
             break;
         }
         i++;
     }
 
-    std::printf("NO");
+    std::printf("%zu", i);
     return 0;
 }
