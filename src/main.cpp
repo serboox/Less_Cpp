bcb97ac1be14e1cdc549595d00370b56d3e75be5
Bukharkin Sergey <buharkin@selectel.ru>
2019-01-02 12:51:17 +0300

    Finished work

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,10 @@ int main()
 {
     int a = 0, b = 0, c = 0, d = 0;
     std::cin >> a >> b >> c >> d;
-    int x = a - c, y = b - d;
-    if ((x >= -1 && x <= 1) && (y >= -1 && y <= 1))
+    int x = 0, y = 0;
+    x = std::abs(a - c);
+    y = std::abs(b - d);
+    if (x == y)
     {
         std::printf("%s", "YES");
         return 0;
