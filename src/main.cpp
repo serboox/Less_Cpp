a085642118a63a57d1af345b0c52db038b8f86d2
Bukharkin Sergey <buharkin@selectel.ru>
2019-01-03 00:36:24 +0300

    Finished work

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     size_t x = 0, i = 0;
-    int max = 0;
+    int max = 0, maxCount = 1;
     std::cin >> max;
     while (true)
     {
@@ -13,13 +13,18 @@ int main()
         {
             break;
         }
+        if (x == max)
+        {
+            maxCount++;
+        }
         if (x > max)
         {
             max = x;
+            maxCount = 1;
         }
         i++;
     }
 
-    std::printf("%d", max);
+    std::printf("%d", maxCount);
     return 0;
 }
