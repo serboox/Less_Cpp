77097b74516fadd56d7aa415dbb6aab68002565c
Bukharkin Sergey <buharkin@selectel.ru>
2019-01-03 02:08:30 +0300

    Finished work

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,30 @@ int main()
 {
     double n = 0;
     std::cin >> n;
-    double sqrt5 = std::sqrt(5);
-    double arg1 = std::pow((1 + sqrt5) / 2, n);
-    //std::printf("arg1: %lf\n", arg1);
+    double i = 0;
+    while (true)
+    {
+        double sqrt5 = std::sqrt(5);
+        double arg1 = std::pow((1 + sqrt5) / 2, i);
+        //std::printf("arg1: %lf\n", arg1);
 
-    double arg2 = std::pow((1 - sqrt5) / 2, n);
-    //std::printf("arg2: %lf\n", arg2);
+        double arg2 = std::pow((1 - sqrt5) / 2, i);
+        //std::printf("arg2: %lf\n", arg2);
 
-    double res = 1 / sqrt5 * arg1 - arg2;
-    res = round(res);
-    std::printf("%d", (int)res);
+        double res = 1 / sqrt5 * arg1 - arg2;
+        res = round(res);
+
+        if (res == n)
+        {
+            std::printf("%d", (int)i);
+            return 0;
+        }
+        else if (res > n)
+        {
+            std::printf("%d", -1);
+            return 0;
+        }
+        i++;
+    }
     return 0;
 }
