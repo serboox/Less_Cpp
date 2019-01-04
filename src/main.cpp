f126a7084349c73e8f21c618fa9e8be61b40d220
Bukharkin Sergey <buharkin@selectel.ru>
2019-01-04 23:09:06 +0300

    Finished work

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,16 @@
 #include <iostream>
+#include <iomanip> //Для подробного вывода вещественных числел
 #include <cmath>
 
+// trunc() - отсекает вещественную часть, например -3.14 будет -3
+// floor() - округляет вниз, например -3.14 будет -4
+// cail() - округляет вверх, например -3.14 будет -3
+// round() - округедние по математическим правилам, например -3.14 будет -3
+// Pi = std::atan(1) * 4
 int main()
 {
-    int prevVal = 0, curVal = 0, nextVal = 0, lastMaxPosition = 0, minMaxRout = -1;
-    size_t i = 1;
-    while (true)
-    {
-        size_t x = 0;
-        std::cin >> x;
-        if (x == 0)
-        {
-            break;
-        }
-        prevVal = curVal;
-        curVal = nextVal;
-        nextVal = x;
-
-        if (curVal > prevVal && curVal > nextVal && i > 2)
-        {
-            if (lastMaxPosition != 0 && minMaxRout == -1)
-            {
-                minMaxRout = i - lastMaxPosition;
-            }
-            else if (lastMaxPosition != 0 && (minMaxRout > (i - lastMaxPosition)))
-            {
-                minMaxRout = i - lastMaxPosition;
-            }
-            if (lastMaxPosition != i)
-            {
-                lastMaxPosition = i;
-            }
-        }
-        i++;
-    }
-
-    if (minMaxRout == -1)
-    {
-        minMaxRout = 0;
-    }
-
-    std::printf("%d", minMaxRout);
+    double x = 0;
+    std::cin >> x;
+    std::printf("%.6lf", x - (int)x);
     return 0;
 }
